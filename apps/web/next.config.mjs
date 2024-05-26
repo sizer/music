e42576bdf702482346a6bb9f627b17be5711eb0b
sizer <shinse.tanaka@gmail.com>
2024-05-26 19:49:06 +0900

    fix broken link on GitHub Pages

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,10 +1,12 @@
 /** @type {import('next').NextConfig} */
 
 const isGhActions = !!process.env.GITHUB_ACTIONS;
+const basePath = isGhActions ? `/${process.env.GITHUB_REPOSITORY.split('/').slice(1)}` : '';
 
 const nextConfig = {
     output: 'export',
-    assetPrefix: isGhActions ? `https://${process.env.GITHUB_REPOSITORY_OWNER}.github.io/${process.env.GITHUB_REPOSITORY.split('/').slice(1)}/` : '',
+    assetPrefix: isGhActions ? `https://${process.env.GITHUB_REPOSITORY_OWNER}.github.io${basePath}/` : '',
+    basePath,
 };
 
 export default nextConfig;
