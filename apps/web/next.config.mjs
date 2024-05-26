9305cd910ef71232568abdae2cfc49706f43a870
sizer <shinse.tanaka@gmail.com>
2024-05-26 19:29:41 +0900

    fix broken asset url on GitHub Pages
    https://docs.github.com/en/actions/learn-github-actions/variables

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
+
+const isGhActions = !!process.env.GITHUB_ACTIONS;
+
 const nextConfig = {
-    output: 'export'
+    output: 'export',
+    assetPrefix: isGhActions ? `https://${process.env.GITHUB_REPOSITORY_OWNER}.github.io/${process.env.GITHUB_REPOSITORY.split('/').slice(1)}/` : '',
 };
 
 export default nextConfig;
